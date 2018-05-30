dfe915cce29610de7e296b8b3ff50ab05681a037
Ron MacNeil <macro@hotmail.com>
2018-05-30 22:48:28 +1000

    Add ExecuteAny*() methods

--- MacroDiagnostics/ProcessExtensions.cs
+++ MacroDiagnostics/ProcessExtensions.cs
@@ -19,13 +19,93 @@ ProcessExtensions
 {
 
 
+static
+ProcessExtensions()
+{
+    if (EnvironmentExtensions.IsWindows)
+    {
+        ExecuteAnyWrapper = "cmd";
+        ExecuteAnyWrapperArgs = new[]{ "/c" };
+    }
+    else
+    {
+        ExecuteAnyWrapper = "env";
+        ExecuteAnyWrapperArgs = new string[0];
+    }
+}
+
+
+static string
+ExecuteAnyWrapper { get; }
+
+
+static IEnumerable<string>
+ExecuteAnyWrapperArgs { get; }
+
+
 /// <summary>
-/// Run a .NET program, capturing its output
+/// Run any kind of program, possibly from the system path
 /// </summary>
 ///
 /// <remarks>
-/// If the current process is running under a .NET framework "driver" program such as <c>dotnet</c> or <c>mono</c>, it
-/// will be used to run the specified program.
+/// Compared to <see cref="Execute"/>, this method can run more types of programs (e.g. batch files on Windows) and can
+/// find them on the system path.  It uses <c>cmd /c</c> on Windows systems and <c>env</c> on Unix systems.
+/// </remarks>
+///
+/// <param name="echoCommandLine">
+/// Echo full command line to stderr?
+/// </param>
+///
+/// <param name="echoOutput">
+/// Echo stdout and stderr output?
+/// </param>
+///
+/// <param name="workingDirectory">
+/// Absolute path to existent working directory
+/// - OR -
+/// <c>null</c> to use the current process's working directory
+/// </param>
+///
+/// <param name="fileName">
+/// Name of the program to run
+/// - OR -
+/// Full path to the program to run
+/// </param>
+///
+/// <param name="arguments">
+/// Arguments to pass to the program.  Those containing space characters will be quoted, unless they also contain quote
+/// characters in which case they are assumed to be pre-quoted.
+/// </param>
+///
+/// <returns>
+/// The exit code from the program
+/// </returns>
+///
+public static int
+ExecuteAny(
+    bool echoCommandLine,
+    bool echoOutput,
+    string workingDirectory,
+    string fileName,
+    params string[] arguments)
+{
+    arguments = Enumerable.Empty<string>()
+        .Concat(ExecuteAnyWrapperArgs)
+        .Concat(new[]{ fileName })
+        .Concat(arguments)
+        .ToArray();
+    fileName = ExecuteAnyWrapper;
+    return Execute(echoCommandLine, echoOutput, workingDirectory, fileName, arguments);
+}
+
+
+/// <summary>
+/// Run any kind of program, possibly from the system path, capturing its output
+/// </summary>
+///
+/// <remarks>
+/// Compared to <see cref="Execute"/>, this method can run more types of programs (e.g. batch files on Windows) and can
+/// find them on the system path.  It uses <c>cmd /c</c> on Windows systems and <c>env</c> on Unix systems.
 /// </remarks>
 ///
 /// <param name="echoCommandLine">
@@ -58,19 +138,19 @@ ProcessExtensions
 /// </returns>
 ///
 public static ProcessExecuteResult
-ExecuteDotnetCaptured(
+ExecuteAnyCaptured(
     bool echoCommandLine,
     bool echoOutput,
     string workingDirectory,
     string fileName,
     params string[] arguments)
 {
-    if (EnvironmentExtensions.DotnetProgram != null)
-    {
-        arguments = new[]{ fileName }.Concat(arguments).ToArray();
-        fileName = EnvironmentExtensions.DotnetProgram;
-    }
-
+    arguments = Enumerable.Empty<string>()
+        .Concat(ExecuteAnyWrapperArgs)
+        .Concat(new[]{ fileName })
+        .Concat(arguments)
+        .ToArray();
+    fileName = ExecuteAnyWrapper;
     return ExecuteCaptured(echoCommandLine, echoOutput, workingDirectory, fileName, arguments);
 }
 
@@ -80,8 +160,8 @@ ExecuteDotnetCaptured(
 /// </summary>
 ///
 /// <remarks>
-/// If the current process is running under a .NET framework "driver" program such as <c>dotnet</c> or <c>mono</c>, it
-/// will be used to run the specified program.
+/// If the current process is running under a .NET framework "driver" program such as <c>dotnet</c> or <c>mono</c>, the
+/// same one will be used to run the program.
 /// </remarks>
 ///
 /// <param name="echoCommandLine">
@@ -99,9 +179,7 @@ ExecuteDotnetCaptured(
 /// </param>
 ///
 /// <param name="fileName">
-/// Filename of the program to run, if it is on the system <c>path</c>
-/// - OR -
-/// Full path to the program to run
+/// Full path to the .NET program to run
 /// </param>
 ///
 /// <param name="arguments">
@@ -132,9 +210,14 @@ ExecuteDotnet(
 
 
 /// <summary>
-/// Run a program, capturing its output
+/// Run a .NET program, capturing its output
 /// </summary>
-/// 
+///
+/// <remarks>
+/// If the current process is running under a .NET framework "driver" program such as <c>dotnet</c> or <c>mono</c>, the
+/// same one will be used to run the specified program.
+/// </remarks>
+///
 /// <param name="echoCommandLine">
 /// Echo full command line to stderr?
 /// </param>
@@ -150,9 +233,111 @@ ExecuteDotnet(
 /// </param>
 ///
 /// <param name="fileName">
-/// Filename of the program to run, if it is on the system <c>path</c>
+/// Full path to the .NET program to run
+/// </param>
+///
+/// <param name="arguments">
+/// Arguments to pass to the program.  Those containing space characters will be quoted, unless they also contain quote
+/// characters in which case they are assumed to be pre-quoted.
+/// </param>
+///
+/// <returns>
+/// A <see cref="ProcessExecuteResult"/> containing the program's output and exit code
+/// </returns>
+///
+public static ProcessExecuteResult
+ExecuteDotnetCaptured(
+    bool echoCommandLine,
+    bool echoOutput,
+    string workingDirectory,
+    string fileName,
+    params string[] arguments)
+{
+    if (EnvironmentExtensions.DotnetProgram != null)
+    {
+        arguments = new[]{ fileName }.Concat(arguments).ToArray();
+        fileName = EnvironmentExtensions.DotnetProgram;
+    }
+
+    return ExecuteCaptured(echoCommandLine, echoOutput, workingDirectory, fileName, arguments);
+}
+
+
+/// <summary>
+/// Run a native binary executable program
+/// </summary>
+///
+/// <param name="echoCommandLine">
+/// Echo full command line to stderr?
+/// </param>
+///
+/// <param name="echoOutput">
+/// Echo stdout and stderr output?
+/// </param>
+///
+/// <param name="workingDirectory">
+/// Absolute path to existent working directory
 /// - OR -
+/// <c>null</c> to use the current process's working directory
+/// </param>
+///
+/// <param name="fileName">
 /// Full path to the program to run
+/// </param>
+///
+/// <param name="arguments">
+/// Arguments to pass to the program.  Those containing space characters will be quoted, unless they also contain quote
+/// characters in which case they are assumed to be pre-quoted.
+/// </param>
+///
+/// <returns>
+/// The exit code from the program
+/// </returns>
+///
+public static int
+Execute(
+    bool echoCommandLine,
+    bool echoOutput,
+    string workingDirectory,
+    string fileName,
+    params string[] arguments)
+{
+    Action<string> onCommandLine = null;
+    Action<string> onStandardOutput = null;
+    Action<string> onErrorOutput = null;
+    if (echoCommandLine)
+    {
+        onCommandLine = s => Console.Error.WriteLine(s);
+    }
+    if (echoOutput)
+    {
+        onStandardOutput = s => Console.Out.WriteLine(s);
+        onErrorOutput = s => Console.Error.WriteLine(s);
+    }
+    return Execute(onCommandLine, onStandardOutput, onErrorOutput, workingDirectory, fileName, arguments);
+}
+
+
+/// <summary>
+/// Run a native binary executable program, capturing its output
+/// </summary>
+///
+/// <param name="echoCommandLine">
+/// Echo full command line to stderr?
+/// </param>
+///
+/// <param name="echoOutput">
+/// Echo stdout and stderr output?
+/// </param>
+///
+/// <param name="workingDirectory">
+/// Absolute path to existent working directory
+/// - OR -
+/// <c>null</c> to use the current process's working directory
+/// </param>
+///
+/// <param name="fileName">
+/// Full path to the native binary executable program to run
 /// </param>
 ///
 /// <param name="arguments">
@@ -202,63 +387,6 @@ ExecuteCaptured(
         errorOutput.ToString(),
         combinedOutput.ToString(),
         exitCode);
-}
-
-
-/// <summary>
-/// Run a program
-/// </summary>
-/// 
-/// <param name="echoCommandLine">
-/// Echo full command line to stderr?
-/// </param>
-///
-/// <param name="echoOutput">
-/// Echo stdout and stderr output?
-/// </param>
-///
-/// <param name="workingDirectory">
-/// Absolute path to existent working directory
-/// - OR -
-/// <c>null</c> to use the current process's working directory
-/// </param>
-///
-/// <param name="fileName">
-/// Filename of the program to run, if it is on the system <c>path</c>
-/// - OR -
-/// Full path to the program to run
-/// </param>
-///
-/// <param name="arguments">
-/// Arguments to pass to the program.  Those containing space characters will be quoted, unless they also contain quote
-/// characters in which case they are assumed to be pre-quoted.
-/// </param>
-///
-/// <returns>
-/// The exit code from the program
-/// </returns>
-///
-public static int
-Execute(
-    bool echoCommandLine,
-    bool echoOutput,
-    string workingDirectory,
-    string fileName,
-    params string[] arguments)
-{
-    Action<string> onCommandLine = null;
-    Action<string> onStandardOutput = null;
-    Action<string> onErrorOutput = null;
-    if (echoCommandLine)
-    {
-        onCommandLine = s => Console.Error.WriteLine(s);
-    }
-    if (echoOutput)
-    {
-        onStandardOutput = s => Console.Out.WriteLine(s);
-        onErrorOutput = s => Console.Error.WriteLine(s);
-    }
-    return Execute(onCommandLine, onStandardOutput, onErrorOutput, workingDirectory, fileName, arguments);
 }
 
 
